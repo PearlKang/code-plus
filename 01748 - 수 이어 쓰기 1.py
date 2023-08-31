c46db7ef194a82a16d711fa16e26ade5925b99aa
BenKang <rksk1588@gmail.com>
2023-09-01 01:28:49 +0900

    2023.09.01

---
+++ 01748 - 수 이어 쓰기 1.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+ans = 0
+start = 1
+length = 1
+
+while start <= N:
+    end = start * 10 - 1
+
+    if end > N:
+        end = N
+
+    ans += (end - start + 1) * length
+    start *= 10
+    length += 1
+
+print(ans)
