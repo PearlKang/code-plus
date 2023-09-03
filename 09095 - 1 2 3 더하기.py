c61ae4703abbc4a5216c65b597959d5e711add32
BenKang <rksk1588@gmail.com>
2023-09-03 17:39:43 +0900

    2023.09.03

---
+++ 09095 - 1 2 3 더하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def go(sum, goal):
+    if sum > goal:
+        return 0
+    if sum == goal:
+        return 1
+    now = 0
+    for i in range(1,4):
+        now += go(sum+i, goal)
+    return now
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(go(0, n))
